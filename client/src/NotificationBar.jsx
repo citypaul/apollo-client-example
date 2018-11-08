e6b630e73a568ebaf98a4d2d5ce4bfba01ae2c8a
Paul Hammond <paul.hammond@gmail.com>
2018-11-08 10:47:27 +0000

    wip

--- client/src/NotificationBar.jsx
+++ client/src/NotificationBar.jsx
@@ -1,5 +1,48 @@
-import React from 'react'
+// import React from 'react'
+// import { Mutation } from 'react-apollo'
+// import gql from 'graphql-tag'
+// import './notification-bar.css'
 
+// const mutation = gql`
+//   mutation countQuery($count: Int) {
+//     count(count: $count) @client {
+//       count
+//     }
+//   }
+// `
+
+// export default () => (
+//   <Mutation mutation={mutation}>
+//     {(count, { data }) => {
+//       console.log(count)
+//       console.log(data)
+//       return <div className="notification-bar" />
+//     }}
+//   </Mutation>
+// )
+
+import React from 'react'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 import './notification-bar.css'
 
-export default props => <div className="notification-bar">Local state notifications</div>
+const query = gql`
+  query countQuery($count: Int) {
+    count(count: $count) @client {
+      number
+    }
+  }
+`
+
+export default () => (
+  <Query query={query}>
+    {({ data, loading }) => {
+      console.log(data)
+      if (loading) {
+        return <h1>loading...</h1>
+      }
+
+      return <div className="notification-bar">{data.count.number}</div>
+    }}
+  </Query>
+)
--- client/src/apollo-client-local-state-setup.js
+++ client/src/apollo-client-local-state-setup.js
@@ -1,12 +1,36 @@
 // docs: https://www.apollographql.com/docs/link/links/state.html
 import { withClientState } from 'apollo-link-state'
 
+const typeDefs = `
+  type Count {
+    number: Int!
+  }
+
+  type Query {
+    count: Count!
+  }
+
+  type Mutation {
+    incrementCount(number: Int): Count
+    decrementCount(number: Int): Count
+  }
+`
+
+const defaults = {
+  count: {
+    number: 1,
+    __typename: 'Count',
+  },
+}
+
 export default cache =>
   withClientState({
     cache,
+    typeDefs,
+    defaults,
     resolvers: {
       Mutation: {
-        count: (_, { count }, { cache }) => {
+        count: (_, { count = 0 }, { cache }) => {
           const data = {
             __typename: 'Count',
             count,
